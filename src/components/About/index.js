dace418a1e805af700c79e4596fe48bfec24a363
Rogelio <rogeliogalicia@Rogelios-Air.lan>
2021-09-21 16:15:25 -0500

    pic left/ p right

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,12 +3,12 @@ import ProfileImg from "../../assets/cover/profile-image.jpeg";
 
 function About() {
   return (
-    <section>
+    <section className="container">
       <h2 id="about">ABOUT ME</h2>
       <hr />
         <img src={ProfileImg} alt="Portrait" />
         <p>
-          Full stack web developer with a coding certificate from UC Berkeley, Currently working on a mechanical engineering degree with a minor in computer science. 
+          Full stack web developer with a coding certificate from UC Berkeley. Born in Mexico City and raised in San Francisco, CA. Currently based in Chicago, IL. 
         </p>
     </section>
   );
